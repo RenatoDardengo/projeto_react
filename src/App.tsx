a49988d17e39f3a36e9141adf2eb68dd15e8e844
Renato Dardengo <rdardengo01@gmail.com>
2023-06-01 21:38:58 -0300

    Estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  var name:string="Renato";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header>
+        <h1> Seja bem vindo {name}</h1>
+      
       </header>
     </div>
   );
