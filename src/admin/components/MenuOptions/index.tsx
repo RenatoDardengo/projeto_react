4c25deeddea3def8e26e54e75d5a1e289a5eb913
Renato Dardengo <rdardengo01@gmail.com>
2023-06-19 23:04:22 -0300

    Implementando menuOptions

--- src/admin/components/MenuOptions/index.tsx
+++ src/admin/components/MenuOptions/index.tsx
@@ -3,23 +3,39 @@ import './style.css';
 import menuSale from "../../assets/img/cart.png";
 import homeIcon from "../../assets/img/home.png";
 
-const MenuOptions=()=>{
+
+
+const MenuOptions = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const handleMenuClick = (menu: string) => {
+        if (menu === 'Vendas') {
+            setDropdownOpen(!dropdownOpen);
+        } else {
+            setDropdownOpen(false);
+        }
+        setSelectedMenu(menu);
+    };
+
+    const handleMenuCollapse = () => {
+        setDropdownOpen(false);
+    };
+
     return (
         <div>
-            <div className="sidebar_item">
+            <div className={`sidebar_item ${selectedMenu === 'Inicio' ? 'selected' : ''}`}>
                 <img src={homeIcon} alt="home" />
-                <span>Inicio</span>
+                <span onClick={() => { handleMenuCollapse(); handleMenuClick('Inicio') }}>Inicio</span>
             </div>
-            <div className="sidebar_item">
+            <div className={`sidebar_item ${selectedMenu === 'Vendas' ? 'selected' : ''}`}>
                 <img src={menuSale} alt="Vendas" />
-                <span onClick={toggleDropdown}>Vendas</span>
-                {dropdownOpen && (
+                <span onClick={() => { handleMenuCollapse(); handleMenuClick('Vendas') }}>Vendas</span>
+                {dropdownOpen && selectedMenu === 'Vendas' && (
                     <div className="dropdown_menu">
                         <ul>
                             <li>Opção 1</li>
@@ -31,21 +47,22 @@ const MenuOptions=()=>{
             </div>
             {dropdownOpen ? (
                 <>
-                    <div className="sidebar_item empty_space"></div>
-                    <div className="sidebar_item">
+                    <div className=" empty_space"></div>
+                    <div className={`sidebar_item ${selectedMenu === 'Orçamentos' ? 'selected' : ''}`}>
                         <img src={require("../../assets/img/list.png")} alt="Orçamentos" />
-                        <span>Orçamentos</span>
+                        <span onClick={() => { handleMenuCollapse(); handleMenuClick('Orçamentos') }}>Orçamentos</span>
                     </div>
                 </>
             ) : (
-                <div className="sidebar_item">
+                <div className={`sidebar_item ${selectedMenu === 'Orçamentos' ? 'selected' : ''}`}>
                     <img src={require("../../assets/img/list.png")} alt="Orçamentos" />
-                    <span>Orçamentos</span>
+                    <span onClick={() => { handleMenuCollapse(); handleMenuClick('Orçamentos') }}>Orçamentos</span>
                 </div>
             )}
         </div>
     );
-
 };
 
 export default MenuOptions;
+
+
