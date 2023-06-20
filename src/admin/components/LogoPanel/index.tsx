4883490b700138975672b7f55c7a9b45e67ff066
Renato Dardengo <rdardengo01@gmail.com>
2023-06-19 21:17:04 -0300

    logo

--- src/admin/components/LogoPanel/index.tsx
+++ src/admin/components/LogoPanel/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import './style.css'
+import './style.css';
+import logo from "../../assets/img/logo.png";
 const LogoPanel = ()=>{
     return (
         <div className="LogoPanel">
-            Imagem
+            <img
+                src={logo}
+                alt="logo"
+                
+
+            />
         </div>
     )
 };
